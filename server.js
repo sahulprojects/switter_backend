10918187d374b9ea3d861f0c7428476bdedf78f0
sahulhameed <thisissahulhameed>
2022-04-02 12:16:51 +0530

    deploy changes

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(body_parser.urlencoded({ extended: false }));
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.alht5.mongodb.net/SwitterDB?retryWrites=true&w=majority`
+    `mongodb+srv://sahul:CyvL5VL719QVZ1YN@cluster0.alht5.mongodb.net/SwitterDB?retryWrites=true&w=majority`
   )
   .then(() => console.log("Db is connected"))
   .catch((err) => console.log(err, "it has an error"));
